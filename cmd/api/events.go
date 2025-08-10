7dc7981686e4de05cc23cc52d7f8972596f27880
Dhireen Kumar Rajak <iamdeerajak@gmail.com>
2025-08-10 23:00:40 +0530

    Added BLL for events handelers

--- cmd/api/events.go
+++ cmd/api/events.go
@@ -16,7 +16,7 @@ func (app *application) createEvent(c *gin.Context) {
 		return
 	}
 
-	err := app.models.Events.insert(&event)
+	err := app.models.Events.Insert(&event)
 
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create event"})
@@ -26,7 +26,23 @@ func (app *application) createEvent(c *gin.Context) {
 	c.JSON(http.StatusCreated, event)
 }
 
+func (app *application) getAllEvents(c *gin.Context) {
+	events, err := app.models.Events.GetAll()
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrive "})
+	}
+
+	c.JSON(http.StatusOK, events)
+}
+
 func (app *application) getEvent(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid event ID"})
+	}
+
 	event, err := app.models.Events.Get(id)
 
 	if event == nil {
@@ -73,4 +89,17 @@ func (app *application) updateEvent(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, updatedEvent)
+}
+
+func (app *application) deleteEvent(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid event ID"})
+	}
+
+	if err := app.models.Events.Delete(id); err != nil { 
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete event"})
+	}
+
+	c.JSON(http.StatusNoContent, nil)
 }
